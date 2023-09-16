9a22dd953e37b2a02fe8e90a59dfb908bead65a0
tamo-tmry <mollianium@gmail.com>
2023-09-17 02:36:03 +0900

    feat: 🎸 学習したことを入力する画面の追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      apiKey: process.env.OPENAI_API_KEY,
+      model: process.env.OPENAI_MODEL,
+      systemMessage: process.env.SYSTEM_MESSAGE,
+    }
+  }
 })
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <p>今日学んだことを書いてね</p>
+    <ol>
+        <li v-for="(talk) in talks">{{ talk.content }}</li>
+    </ol>
+    <textarea :disabled="isRequesting" v-model="learned" cols="30" rows="10"></textarea>
+    <input :disabled="isRequesting" type="button" value="登録" @click="register">
+</template>
+
+<script setup lang="ts">
+import {ref} from '#imports'
+import OpenAI from 'openai'
+const learned = ref('')
+const isRequesting = ref(false)
+const runtimeConfig = useRuntimeConfig()
+const talkList = ref<any[]>([{ role: 'system', content: runtimeConfig.public.systemMessage }])
+
+const openai = new OpenAI({
+    apiKey: runtimeConfig.public.apiKey,
+    dangerouslyAllowBrowser: true
+})
+
+const register = async () => {
+    isRequesting.value = true
+    const completion = await openai.chat.completions.create({
+        messages: [...talkList.value, { role: 'user', content: learned.value }],
+        model: runtimeConfig.public.model,
+    });
+    talkList.value.push({ role: 'user', content: learned.value })
+    talkList.value.push(completion.choices[0].message)
+    learned.value = ''
+    isRequesting.value = false
+}
+
+const talks = computed(() => {
+    return talkList.value.filter((talk) => talk.role !== 'system')
+})
+
+</script>
