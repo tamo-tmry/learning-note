bef44b4293f59639ecfc7bbe3af91694311b9643
tamo-tmry <mollianium@gmail.com>
2023-09-22 23:37:44 +0900

    refactor: 入力部分をコンポーネント化

---
+++ components/MessageForm.vue
@@ -0,0 +1,28 @@
+<template>
+    <VTextarea :disabled="isRequesting" v-model="learned" rows="10" />
+    <VBtn :disabled="isRequesting" @click="register">登録</VBtn>
+    <VBtn @click="gotoCompletePage">終了</VBtn>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const learned = ref('')
+const props = defineProps({
+    isRequesting: Boolean,
+})
+
+const emits = defineEmits<{
+    (e: 'register', text: string): void
+    (e: 'gotoCompletePage'): void
+}>()
+
+const register = () => {
+    emits('register', learned.value)
+    learned.value = ''
+}
+
+const gotoCompletePage = () => {
+    emits('gotoCompletePage')
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,51 @@
 <template>
     <p>今日学んだことを書いてね</p>
     <MessageContainer v-show="hasTalk" :messages="messages" />
-    <VTextarea :disabled="isRequesting" v-model="learned" rows="10" />
-    <VBtn :disabled="isRequesting" @click="register">登録</VBtn>
-    <VBtn @click="gotoCompletePage">終了</VBtn>
+    <MessageForm
+        :isRequesting="isRequesting"
+        @register="register"
+        @gotoCompletePage="gotoCompletePage"
+    />
 </template>
 
 <script setup lang="ts">
-import {ref} from '#imports'
+import { ref } from '#imports'
 import OpenAI from 'openai'
 import { MessageType } from '~/types/Message'
 const learned = ref('')
 const isRequesting = ref(false)
 const runtimeConfig = useRuntimeConfig()
-const talkList = ref<any[]>([{ role: 'system', content: runtimeConfig.public.systemMessage }])
+const talkList = ref<any[]>([
+    { role: 'system', content: runtimeConfig.public.systemMessage },
+])
 
 const MAX_REQUEST_COUNT = 3
 
 const openai = new OpenAI({
     apiKey: runtimeConfig.public.apiKey,
-    dangerouslyAllowBrowser: true
+    dangerouslyAllowBrowser: true,
 })
 
-const register = async () => {
+const register = async (learnedText: string) => {
     isRequesting.value = true
     const completion = await openai.chat.completions.create({
-        messages: [...talkList.value, { role: 'user', content: learned.value }],
+        messages: [...talkList.value, { role: 'user', content: learnedText }],
         model: runtimeConfig.public.model,
-    });
-    talkList.value.push({ role: 'user', content: learned.value })
+    })
+    talkList.value.push({ role: 'user', content: learnedText })
     talkList.value.push(completion.choices[0].message)
-    learned.value = ''
     isRequesting.value = false
 }
 
 const messages = computed((): MessageType[] => {
-    return talkList.value.filter((talk) => talk.role !== 'system').map((talk) => {
-        return {
-            text: talk.content,
-            isMine: talk.role === 'user'
-        }
-    })
+    return talkList.value
+        .filter((talk) => talk.role !== 'system')
+        .map((talk) => {
+            return {
+                text: talk.content,
+                isMine: talk.role === 'user',
+            }
+        })
 })
 
 const hasTalk = computed(() => {
